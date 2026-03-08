f33fd406713601b4cb0cc83f29f20aed9c0b6d3f
odiloviccc <odiloviccc@gmail.com>
2026-03-09 01:22:45 +0500

    [-] Removed unused files

--- tunnel-mirasoft.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Mirasoft tunnel — mirasoft.pyramide.uz → localhost:8000
-cloudflared tunnel --config ~/.cloudflared/mirasoft.pyramide.uz.yml run
--- tunnel-webmini.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Vektron tunnel — webmini.pyramide.uz → localhost:3000
-cloudflared tunnel --config ~/.cloudflared/webmini.yml run
